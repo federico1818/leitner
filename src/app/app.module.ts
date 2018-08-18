abacbdc99a49ff54c0afcc9d9c71a20c7a6a8d24
charly300 <federico1818@gmail.com>
2018-08-17 23:18:58 -0300

    generate tests module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser'
 import { NgModule } from '@angular/core'
 
+import { AngularFireModule } from 'angularfire2'
+import { AngularFirestoreModule } from 'angularfire2/firestore'
+import { environment } from '../environments/environment'
+
 import { AppRoutingModule } from './app-routing.module'
-import { AppComponent } from './app.component'
+
 import { SharedModule } from './shared/shared.module'
 import { CardsModule } from './cards/cards.module'
 import { BoxesModule } from './boxes/boxes.module'
+import { TestsModule } from './tests/tests.module'
 
-import { AngularFireModule } from 'angularfire2'
-import { AngularFirestoreModule } from 'angularfire2/firestore'
-import { environment } from '../environments/environment';
+import { AppComponent } from './app.component'
 import { MenuComponent } from './menu/menu.component'
 
 @NgModule({
@@ -22,6 +25,7 @@ import { MenuComponent } from './menu/menu.component'
         SharedModule,
         BoxesModule,
         CardsModule,
+        TestsModule,
         AppRoutingModule,
         AngularFireModule.initializeApp(environment.firebase),
         AngularFirestoreModule
---
+++ src/app/tests/tests.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core'
+import { CommonModule } from '@angular/common'
+
+import { TestsRoutingModule } from './tests-routing.module'
+
+@NgModule({
+    imports: [
+        CommonModule,
+        TestsRoutingModule
+    ],
+    declarations: []
+})
+
+export class TestsModule { }
