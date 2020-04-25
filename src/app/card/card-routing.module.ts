3cd1bf8c1a2e7fc917bafad0187cf896cf072bfa
Federico González <federico1818@gmail.com>
2020-04-25 03:18:49 -0300

    create form component

--- src/app/card/card-routing.module.ts
+++ src/app/card/card-routing.module.ts
@@ -2,11 +2,16 @@ import { NgModule } from '@angular/core'
 import { Routes, RouterModule } from '@angular/router'
 
 import { CardComponent } from './pages/card/card.component'
+import { CardCreateComponent } from './pages/card-create/card-create.component'
 
 const routes: Routes = [
     {
         path: 'cards',
         component: CardComponent
+    },
+    {
+        path: 'cards/create',
+        component: CardCreateComponent
     }
 ]
 
--- src/app/card/card.module.ts
+++ src/app/card/card.module.ts
@@ -1,13 +1,22 @@
 import { NgModule } from '@angular/core'
 import { CommonModule } from '@angular/common'
 
-import { CardRoutingModule } from './card-routing.module';
+import { MaterialModule } from './../material/material.module'
+
+import { CardRoutingModule } from './card-routing.module'
 import { CardComponent } from './pages/card/card.component'
+import { CardCreateComponent } from './pages/card-create/card-create.component';
+import { CardFormComponent } from './components/card-form/card-form.component'
 
 @NgModule({
-    declarations: [CardComponent],
+    declarations: [
+        CardComponent, 
+        CardCreateComponent, 
+        CardFormComponent
+    ],
     imports: [
         CommonModule,
+        MaterialModule,
         CardRoutingModule
     ]
 })
